f833fa7bbd8fceae564619750196029aae213bf5
André Fernandes <andrefernandes6@gmail.com>
2011-09-30 13:53:46 +0100

    rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CmovAssignment1Ruby::Application.config.secret_token = '763852701162283de3945e4ca6f5314a65a061d3a514f280784e6189f8a9726625ec9a1f676a1e459a7021f759331cd315985c1783d5eb64e13edf39cf2b18ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CmovAssignment1Ruby::Application.config.session_store :cookie_store, key: '_cmov-assignment1-ruby_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CmovAssignment1Ruby::Application.config.session_store :active_record_store
