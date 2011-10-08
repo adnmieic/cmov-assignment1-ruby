760308a8fe64e63a78569af21671635bfa56b933
André Fernandes <andrefernandes6@gmail.com>
2011-10-08 12:23:10 +0100

    json formating example for Specialty

--- app/models/specialty.rb
+++ app/models/specialty.rb
@@ -1,3 +1,8 @@
 class Specialty < ActiveRecord::Base
-	has_and_belongs_to_many :doctors
+  has_and_belongs_to_many :doctors
+
+  def as_json(options = {})
+    options = { :only => [:id, :name] }
+    super(options)
+  end
 end
