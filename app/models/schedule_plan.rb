a69945cf17e6444348e7a65bd170f3b4b94146c6
Diogo Costa <costa.h4evr@gmail.com>
2011-10-29 16:51:28 +0100

    Added schedule restrictions

--- app/models/schedule_plan.rb
+++ app/models/schedule_plan.rb
@@ -1,7 +1,36 @@
 class SchedulePlan < ActiveRecord::Base
   belongs_to :doctor
   has_many :schedules
-  
+  before_create :check_for_appointments
+  after_save :correct_other_plans
+
+  def check_for_appointments
+    appointment = Appointment.find(:first, :conditions => ["DATE(date) >= ? AND doctor_id = ?", self.start, self.doctor_id])
+
+    if appointment
+      self.errors.add(:start, "Appointments are already scheduled after this date.")
+      return false
+    end
+
+    if self.end < self.start
+      self.errors.add(:end, "End date cannot be before start date!")
+      return false
+    end
+    true
+  end
+
+  def correct_other_plans
+    schedule_plans = SchedulePlan.find(:all, :conditions => ["doctor_id = ? AND id <> ? AND (end IS NULL OR DATE(end) >= ?)", self.doctor_id, self.id, self.start])
+    
+    logger.debug schedule_plans.inspect
+
+    schedule_plans.each do |schedule_plan|
+      logger.debug self.start.yesterday.inspect
+      schedule_plan.end = self.start.yesterday;
+      schedule_plan.save
+    end
+  end
+
   def as_json(options = {})
     options = { :only => [:id, :start, :end, :doctor_id] }
     super(options)
